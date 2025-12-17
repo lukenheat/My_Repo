b6eb5d87f8f058239f222a3d7a9d9456d2cc24b9
lukenheat <113115293+lukenheat@users.noreply.guthub.com>
2025-12-17 14:29:58 -0500

    test error 3

--- scripts/lesson02-Components.R
+++ scripts/lesson02-Components.R
@@ -46,7 +46,7 @@ plot5 = ggplot( data=weatherData ) +
         subtitle="Lansing, MI -- 2016",
         x = "Average Temperatures (Fahrenheit)",
         y = "Relative Humidity") +
-  scale_x_continuous( breaks = seq(from=10, to=80, by=10) ) +
+  scale_x_continuous( breaks = seq(from=20, to=90, by=10) ) +
   theme( axis.text.x=element_text(angle=90, vjust=0.5) ) +
   theme_bw();
 plot(plot5);
